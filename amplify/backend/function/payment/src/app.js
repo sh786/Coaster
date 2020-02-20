ff77de3981ce412ff73a30826c94b3bfee044385
Sam Hamburger <samhamburger@Sams-MBP-2.lan1>
2020-02-19 21:51:24 -0500

    mock successful checkout

--- amplify/backend/function/payment/src/app.js
+++ amplify/backend/function/payment/src/app.js
@@ -1,105 +1,81 @@
-/*
-Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-    http://aws.amazon.com/apache2.0/
-or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
-*/
-
-
-
-
-var express = require('express')
-var bodyParser = require('body-parser')
-var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
-var braintree = require("braintree");
+var express = require('express');
+var bodyParser = require('body-parser');
+var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
+var braintree = require('braintree');
 
 var gateway = braintree.connect({
   environment: braintree.Environment.Sandbox,
-  merchantId: "rj9z33zwvkm4n6sm",
-  publicKey: "8t6mkc9cqgt4qpfy",
-  privateKey: "4b15007dd0def9e920590d0edfe4c4fd"
+  merchantId: 'rj9z33zwvkm4n6sm',
+  publicKey: '8t6mkc9cqgt4qpfy',
+  privateKey: '4b15007dd0def9e920590d0edfe4c4fd',
 });
 
 // declare a new express app
-var app = express()
-app.use(bodyParser.json())
-app.use(awsServerlessExpressMiddleware.eventContext())
+var app = express();
+app.use(bodyParser.json());
+app.use(awsServerlessExpressMiddleware.eventContext());
 
 // Enable CORS for all methods
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
 });
-
 
 /**********************
  * Example get method *
  **********************/
 
 app.get('/client-token', async function(req, res) {
-  gateway.clientToken.generate({
-    // customerId: "testCustomerId"
-  }, function (err, response) {
-    res.send({success: 'hit client-token endpoint', token: response.clientToken});
-  });
-});
+  const response = await gateway.clientToken.generate({});
 
-app.get('/items/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
+  if (!response.success) {
+    res.error({
+      error: 'could not create client-token',
+    });
+  } else {
+    console.log(`client-token: ${response.clientToken}`);
+    res.json({
+      success: 'successfully created client-token!',
+      url: req.url,
+      token: response.clientToken,
+    });
+  }
 });
 
 /****************************
-* Example post method *
-****************************/
+ * Example post method *
+ ****************************/
 
-app.post("/checkout", function (req, res) {
-  var nonceFromTheClient = req.body.payment_method_nonce;
+app.post('/checkout', async function(req, res) {
+  // const nonceFromTheClient = req.body.payment_method_nonce;
+  // below from Testing section of Briantree docs
+  const nonceFromTheClient = 'fake-valid-nonce';
+
   // Use payment method nonce here
-  console.log(req.body)
-  res.send({success: 'post call succeeded'});
-});
+  console.log(`nonce: ${nonceFromTheClient}`);
 
-app.post('/items/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
-});
+  const response = await gateway.transaction.sale({
+    amount: '10.00',
+    paymentMethodNonce: nonceFromTheClient,
+    // deviceData: deviceDataFromTheClient,
+    options: {
+      submitForSettlement: true,
+    },
+  });
+  console.log(`response: ${JSON.stringify(response)}`);
 
-/****************************
-* Example put method *
-****************************/
-
-app.put('/items', function(req, res) {
-  // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
-});
-
-app.put('/items/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
-});
-
-/****************************
-* Example delete method *
-****************************/
-
-app.delete('/items', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
-});
-
-app.delete('/items/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
+  res.json({ success: 'checkout completed', response: response });
 });
 
 app.listen(3000, function() {
-    console.log("App started")
+  console.log('App started');
 });
 
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
 // this file
-module.exports = app
+module.exports = app;
